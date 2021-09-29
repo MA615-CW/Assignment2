97088f076549928791edde6e81754de518c49908
GuestUser <GuestUser@nat-wireless-guest-reg-153-50.bu.edu>
2021-09-29 15:47:36 -0400

    Initial commit of Assignment 2 R project and Wrangling_Code.R

---
+++ Wrangling_code.R
@@ -0,0 +1,5 @@
+#Wrangle the following data sets into "tidy" tibbles
+#Source: https://www.gapminder.org/data/
+
+#First csv file -> children_out_of_school_primary_female.csv
+#Second csv file -> children_per_woman_total_fertility.csv
